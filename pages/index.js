71d5ed8a0685a6a07e254c44dd54ab6d513566e3
Angel Moreno <angelmrsofa@gmail.com>
2021-06-24 00:17:20 -0500

    nextjs-chackra-ui-swr

--- pages/index.js
+++ pages/index.js
@@ -145,7 +145,7 @@ export default function Home() {
         <AlertDialogOverlay>
           <AlertDialogContent>
             <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              Delete Customer
+              Delete Comment
             </AlertDialogHeader>
 
             <AlertDialogBody>
